3fa3008db57bdf97341532ad5251169d394e5028
yanxianUCSB <yanxian@umail.ucsb.edu>
2017-11-17 10:50:54 -0800

    created loadpkgs for gen5 data analysis

---
+++ R/loadpkgs.R
@@ -0,0 +1,14 @@
+#' loadpkgs for gen5 data analysis
+#'
+#' @return
+#' @export
+#'
+#' @examples
+loadpkgs <- function(){
+    library(dplyr)
+    library(ggplot2)
+    devtools::install_github('yanxianucsb/yxhelper')
+    devtools::install_github('yanxianucsb/yxplot')
+    library(yxhelper)
+    library(yxplot)
+}
