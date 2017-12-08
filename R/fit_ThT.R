e3ed6e07e9c7db100865df3d52d6db12962b94c9
yanxianucsb <yanxian@umail.ucsb.edu>
2017-12-08 12:53:55 -0800

    include double boltzmann fitting model

--- R/fit_ThT.R
+++ R/fit_ThT.R
@@ -42,10 +42,10 @@ ui.plot.fit <- function(){
 #' @examples
 filter.fit <- function(df, TOL = 1) {
     df %>%
-        filter(y0.sd < TOL * y0.m,
-               t2.sd < TOL * t2.m,
-               A.sd < TOL * A.m,
-               k.sd < TOL * k.m)
+        filter(abs(y0.sd) < abs(TOL * y0.m),
+               abs(t2.sd) < abs(TOL * t2.m),
+               abs(A.sd) < abs(TOL * A.m),
+               abs(k.sd) < abs(TOL * k.m))
 }
 #' Title
 #'
@@ -92,6 +92,61 @@ fit.boltzmann <- function(ds, start = list(A = 3000, y0 = 1000, k = 10, t2 = 1))
         ) %>%
         ungroup()
 }
+#' Title
+#'
+#' @param ds
+#' @param start
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fit.boltzmann.double <- function(ds, start = list(A = 3000, y0 = 1000, k = 10, t2 = 1,
+                                                  A2 = 3000, k2 = 10, t22 = 1)) {
+    df <- bind_rows(lapply(unique(ds$row), function(ROW){
+        ds1 <- ds %>%
+            filter(row == ROW)
+        bind_rows(lapply(unique(ds1$well), function(WELL){
+            ds2 <- ds1 %>%
+                filter(well == WELL) %>%
+                arrange(rev(desc(realHour))) %>%
+                filter(val.m-3*val.sd < val) %>%  # 3 standard deviation filter
+                filter(val < val.m+3*val.sd) %>%
+                mutate(val = pracma::hampel(val, k = 2, t0 = 3)$y)  # Hampel filter, 3 sigma
+            mod <- Boltzmann_double(ds2$realHour, ds2$val, start = start)
+            ds3 <- ds2 %>% mutate(
+                y0 = coef(mod)['y0'],
+                A = coef(mod)['A'],
+                A2 = coef(mod)['A2'],
+                k = coef(mod)['k'],
+                k2 = coef(mod)['k2'],
+                t2 = coef(mod)['t2'],
+                t22 = coef(mod)['t22'],
+                val.predict = predict(mod, list(x = realHour)))
+        }))
+    }))
+    df <- df %>%
+        group_by(row) %>%
+        mutate(
+            y0.m = mean(y0),
+            y0.sd = sd(y0),
+            A.m = mean(A),
+            A.sd = sd(A),
+            k.m = mean(k),
+            k.sd = sd(k),
+            t2.m = mean(t2),
+            t2.sd = sd(t2),
+            A2.m = mean(A2),
+            A2.sd = sd(A2),
+            k2.m = mean(k2),
+            k2.sd = sd(k2),
+            t22.m = mean(t22),
+            t22.sd = sd(t22),
+            val.pred.m = mean(val.predict),
+            val.pred.sd = sd(val.predict)
+        ) %>%
+        ungroup()
+}
 
 #' Boltzmann model for fitting time series data
 #'
@@ -105,6 +160,25 @@ fit.boltzmann <- function(ds, start = list(A = 3000, y0 = 1000, k = 10, t2 = 1))
 #' @examples
 Boltzmann <- function(time_, val_, start = list(y0 = 500, A = 500, k = 1.1, t2 = 10)) {
     minpack.lm::nlsLM(y ~ y0 + A/(1+exp(-k*(t-t2))), data.frame(t = time_, y = val_),
+                      start = start)
+}
+#' Boltzmann model for fitting time series data
+#'
+#' @param time_
+#' @param val_
+#' @param start
+#'
+#' @return
+#' @export
+#'
+#' @examples
+Boltzmann_double <- function(time_, val_, start = list(y0 = 500,
+                                                       A = 500, k = 1.1, t2 = 10,
+                                                       A2 = 500, k2 = 1.1, t22 = 10)) {
+    minpack.lm::nlsLM(y ~ y0 +
+                          A/(1+exp(-k*(t-t2))) +
+                          A2/(1+exp(-k2*(t-t22))),
+                      data.frame(t = time_, y = val_),
                       start = start)
 }
 #' Fitting ThT time series data with Boltzmann model. return fit data and model
